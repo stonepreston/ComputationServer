e75a186e57065bf1778d6dbcb5cd3d78f5bb21e5
Stone Preston <stonepreston@protonmail.com>
2021-07-18 21:32:55 -0500

    initial commit

---
+++ bin/runtask.bat
@@ -0,0 +1,1 @@
+"/home/stone/julia-1.6.2/bin/julia" --color=yes --depwarn=no --project=@. -q -- "%~dp0..\bootstrap.jl" -r %*
---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+using ComputationServer, Test
+# implement your tests here
+@test 1 == 1
