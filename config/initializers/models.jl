8b05aea94a310655a25245ac650567b3aaf3610a
Stone Preston <stonepreston@protonmail.com>
2021-07-21 23:02:00 -0500

    add some basic routes for model info

--- config/initializers/models.jl
+++ config/initializers/models.jl
@@ -1,1 +1,27 @@
+using Base: String
 using HydraulicModels
+using ModelingToolkit
+
+@parameters t
+
+const static_pipe_key = "Static Pipe"
+const ideal_pressure_source_key  = "Ideal Pressure Source"
+
+const models = Dict{String, Function}(
+    static_pipe_key => StaticPipe,
+    ideal_pressure_source_key => IdealPressureSource
+)
+
+function get_model_keys()
+    return sort(collect(keys(models)), by=x->x[1])
+end
+
+struct ModelCategory 
+    category_key::String
+    model_keys::Vector{String}
+end
+
+const model_categories = ModelCategory[
+    ModelCategory("Pipes", [static_pipe_key]),
+    ModelCategory("Sources", [ideal_pressure_source_key])
+]
--- routes.jl
+++ routes.jl
@@ -1,9 +1,14 @@
 using Genie.Router
+using JSON
 
 route("/") do
   serve_static_file("welcome.html")
 end
 
-route("/hello") do
-  "World!"
+route("/model_keys") do
+  JSON.json(ComputationServer.get_model_keys())
+end
+
+route("/model_categories") do
+  JSON.json(ComputationServer.model_categories)
 end
